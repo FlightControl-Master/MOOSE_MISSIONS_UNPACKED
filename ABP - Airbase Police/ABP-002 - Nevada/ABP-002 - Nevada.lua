237b8d840d157ea117d1d3c54f84c418d51399b2
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-20 18:44:44 +0100

    Clean

--- ABP - Airbase Police/ABP-002 - Nevada/ABP-002 - Nevada.lua
+++ ABP - Airbase Police/ABP-002 - Nevada/ABP-002 - Nevada.lua
@@ -1,3 +1,3 @@
 
 local PlanesClientSet = SET_CLIENT:New():FilterCategories( "plane" ):FilterStart()
-local AirbasePolice = AIRBASEPOLICE_CAUCASUS:New( PlanesClientSet )
+local AirbasePolice = AIRBASEPOLICE_NEVADA:New( PlanesClientSet )
