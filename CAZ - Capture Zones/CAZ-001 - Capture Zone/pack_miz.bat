08cda0da8b590b627fa51134ca7187f9f0e58509
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 13:27:18 +0100

    est

---
+++ CAZ - Capture Zones/CAZ-001 - Capture Zone/pack_miz.bat
@@ -0,0 +1,3 @@
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-001 - Capture Zone/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z a -r -y -tzip "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-001 - Capture Zone/CAZ-001 - Capture Zone.miz" *md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-001 - Capture Zone/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-001 - Capture Zone/_unpacked"
---
+++ CAZ - Capture Zones/CAZ-001 - Capture Zone/unpack_miz.bat
@@ -0,0 +1,2 @@
+del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-001 - Capture Zone/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-001 - Capture Zone/CAZ-001 - Capture Zone.miz" _unpacked/*
