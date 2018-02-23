6641342c71b47c66d63646cbfe07c76d5a3100ed
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 15:59:42 +0100

    ABP

--- ABP - Airbase Police/ABP-001 - Caucasus/unpack_miz.bat
+++ ABP - Airbase Police/ABP-001 - Caucasus/unpack_miz.bat
@@ -1,2 +1,4 @@
 del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
-C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/ABP-001 - Caucasus.miz" _unpacked/*
+md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/ABP-001 - Caucasus.miz" *
--- ABP - Airbase Police/ABP-002 - Nevada/unpack_miz.bat
+++ ABP - Airbase Police/ABP-002 - Nevada/unpack_miz.bat
@@ -1,2 +1,4 @@
 del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-002 - Nevada/_unpacked"
-C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-002 - Nevada/ABP-002 - Nevada.miz" _unpacked/*
+md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-002 - Nevada/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-002 - Nevada/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-002 - Nevada/ABP-002 - Nevada.miz" *
