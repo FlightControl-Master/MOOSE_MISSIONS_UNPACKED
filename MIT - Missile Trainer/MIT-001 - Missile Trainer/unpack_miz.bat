88871a168f28c67cd955fda0e6d2c0d79711003d
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 13:48:20 +0100

    MIT

---
+++ MIT - Missile Trainer/MIT-001 - Missile Trainer/unpack_miz.bat
@@ -0,0 +1,4 @@
+del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/MIT - Missile Trainer/MIT-001 - Missile Trainer/_unpacked"
+md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/MIT - Missile Trainer/MIT-001 - Missile Trainer/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/MIT - Missile Trainer/MIT-001 - Missile Trainer/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/MIT - Missile Trainer/MIT-001 - Missile Trainer/MIT-001 - Missile Trainer.miz" *
