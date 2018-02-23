eed0acf75bc1e4f37d1d79d27d51730bb89d23aa
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 16:07:55 +0100

    ESC

---
+++ ESC - Escorting/ESC-001 - Escorting Helicopters/pack_miz.bat
@@ -0,0 +1,2 @@
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ESC - Escorting/ESC-001 - Escorting Helicopters/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z a -r -y -tzip "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ESC - Escorting/ESC-001 - Escorting Helicopters/ESC-001 - Escorting Helicopters.miz" *
---
+++ ESC - Escorting/ESC-001 - Escorting Helicopters/unpack_miz.bat
@@ -0,0 +1,4 @@
+del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ESC - Escorting/ESC-001 - Escorting Helicopters/_unpacked"
+md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ESC - Escorting/ESC-001 - Escorting Helicopters/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ESC - Escorting/ESC-001 - Escorting Helicopters/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ESC - Escorting/ESC-001 - Escorting Helicopters/ESC-001 - Escorting Helicopters.miz" *
