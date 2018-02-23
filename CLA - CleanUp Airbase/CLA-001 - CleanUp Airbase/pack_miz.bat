5ca84ed4a8f51636a68beecddd662267118d6684
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 16:06:29 +0100

    CLA

---
+++ CLA - CleanUp Airbase/CLA-001 - CleanUp Airbase/pack_miz.bat
@@ -0,0 +1,2 @@
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CLA - CleanUp Airbase/CLA-001 - CleanUp Airbase/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z a -r -y -tzip "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CLA - CleanUp Airbase/CLA-001 - CleanUp Airbase/CLA-001 - CleanUp Airbase.miz" *
