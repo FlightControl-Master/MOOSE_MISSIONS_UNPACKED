de6d52a321a938b18e0e93f78fc6ef62d650da5e
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 13:48:46 +0100

    PAT

---
+++ PAT - Patrolling/PAT-001 - Switching Patrol Zones/pack_miz.bat
@@ -0,0 +1,2 @@
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/PAT - Patrolling/PAT-001 - Switching Patrol Zones/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z a -r -y -tzip "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/PAT - Patrolling/PAT-001 - Switching Patrol Zones/PAT-001 - Switching Patrol Zones.miz" *
---
+++ PAT - Patrolling/PAT-001 - Switching Patrol Zones/unpack_miz.bat
@@ -0,0 +1,4 @@
+del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/PAT - Patrolling/PAT-001 - Switching Patrol Zones/_unpacked"
+md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/PAT - Patrolling/PAT-001 - Switching Patrol Zones/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/PAT - Patrolling/PAT-001 - Switching Patrol Zones/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/PAT - Patrolling/PAT-001 - Switching Patrol Zones/PAT-001 - Switching Patrol Zones.miz" *
