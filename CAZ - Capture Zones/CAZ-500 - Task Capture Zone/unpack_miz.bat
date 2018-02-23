c8f036f6b10ae1fc69464c784e6fe3c721a33059
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-23 16:05:29 +0100

    CAZ

---
+++ CAZ - Capture Zones/CAZ-500 - Task Capture Zone/unpack_miz.bat
@@ -0,0 +1,4 @@
+del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-500 - Task Capture Zone/_unpacked"
+md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-500 - Task Capture Zone/_unpacked"
+cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-500 - Task Capture Zone/_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/CAZ - Capture Zones/CAZ-500 - Task Capture Zone/CAZ-500 - Task Capture Zone.miz" *
