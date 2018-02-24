c35b55f5b970d311a51073447f68a079ca7ef102
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-24 08:40:44 +0100

    Clean

--- SPA - Spawning/SPA-Caucasus/SPA-020 - Ground Ops - Randomize Templates in Random Zones without Waypoints/unpack_miz.bat
+++
@@ -1,4 +0,0 @@
-del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/SPA - Spawning/SPA-Caucasus/SPA-020 - Ground Ops - Randomize Templates in Random Zones without Waypoints/_unpacked"
-md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/SPA - Spawning/SPA-Caucasus/SPA-020 - Ground Ops - Randomize Templates in Random Zones without Waypoints/_unpacked"
-cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/SPA - Spawning/SPA-Caucasus/SPA-020 - Ground Ops - Randomize Templates in Random Zones without Waypoints/_unpacked"
-C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/SPA - Spawning/SPA-Caucasus/SPA-020 - Ground Ops - Randomize Templates in Random Zones without Waypoints/SPA-020 - Ground Ops - Randomize Templates in Random Zones without Waypoints.miz" *
