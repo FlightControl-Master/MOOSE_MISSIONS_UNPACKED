03d4ad99e94e408d9be12face3ab57081c2afcfe
Sven Van de Velde <sven.van.de.velde@telenet.be>
2018-02-24 08:53:20 +0100

    ABP

--- ABP - Airbase Police/ABP-001 - Caucasus/pack_miz.bat
+++ ABP - Airbase Police/ABP-001 - Caucasus/pack_miz.bat
@@ -1,2 +1,2 @@
-cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
-C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z a -r -y -tzip "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/ABP-001 - Caucasus.miz" *
+cd "_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS_UNPACKED/.7z/7z a -r -y -tzip "..$mission_base" *
--- ABP - Airbase Police/ABP-001 - Caucasus/unpack_miz.bat
+++ ABP - Airbase Police/ABP-001 - Caucasus/unpack_miz.bat
@@ -1,4 +1,4 @@
-del /S /Q "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
-md "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
-cd "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/_unpacked"
-C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/.7z/7z x -r -y "C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS/ABP - Airbase Police/ABP-001 - Caucasus/ABP-001 - Caucasus.miz" *
+del /S /Q "_unpacked"
+md "_unpacked"
+cd "_unpacked"
+C:/Users/vandeves/Documents/GitHub/MOOSE_MISSIONS_UNPACKED/.7z/7z x -r -y "..$mission_base" *
