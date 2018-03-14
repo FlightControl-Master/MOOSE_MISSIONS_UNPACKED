dcf94ef9cf8faa1bf2a616b837318229a31c87fd
FlightControl_Master <sven.van.de.velde@telenet.be>
2018-03-14 07:02:32 +0100

    New Missions

--- DES - Designation/DES-900 - NTTR Designation Stress Test/pack_miz.bat
+++
@@ -1,2 +0,0 @@
-cd "_unpacked"
-7z a -r -y -tzip "..\DES-900 - NTTR Designation Stress Test.miz" *
--- DES - Designation/DES-900 - NTTR Designation Stress Test/unpack_miz.bat
+++
@@ -1,4 +0,0 @@
-del /S /Q "_unpacked"
-md "_unpacked"
-cd "_unpacked"
-7z x -r -y "..\DES-900 - NTTR Designation Stress Test.miz" *
