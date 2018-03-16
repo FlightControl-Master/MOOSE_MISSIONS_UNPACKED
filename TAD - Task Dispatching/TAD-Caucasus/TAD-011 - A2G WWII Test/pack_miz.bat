6f278fbb9b98bbbddb8e411d2bc14a6f0ed3196d
FlightControl_Master <sven.van.de.velde@telenet.be>
2018-03-16 06:42:27 +0100

    Mission Updates

--- TAD - Task Dispatching/TAD-Caucasus/TAD-011 - A2G WWII Test/pack_miz.bat
+++
@@ -1,2 +0,0 @@
-cd "_unpacked"
-7z a -r -y -tzip "..\TAD-011 - A2G Task Dispatching WWII Test.miz" *
--- TAD - Task Dispatching/TAD-Caucasus/TAD-106 - A2G DETECTION_AREAS - Split SEAD to BAI and SEAD/pack_miz.bat
+++
@@ -1,2 +0,0 @@
-cd "_unpacked"
-7z a -r -y -tzip "..\TAD-106 - A2G DETECTION_AREAS - Split SEAD to BAI and SEAD.miz" *
