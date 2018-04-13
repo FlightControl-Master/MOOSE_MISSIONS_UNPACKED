da50c630705f06bba8ab947a35c581645f9703e4
FlightControl_Master <sven.van.de.velde@telenet.be>
2018-04-13 22:36:08 +0200

    # Conflicts: #	AIC - AI Cargo/APC - Armoured Personnel Carrier/AIC-APC-000 - APC/_unpacked/l10n/DEFAULT/dictionary #	AIC - AI Cargo/APC - Armoured Personnel Carrier/AIC-APC-000 - APC/_unpacked/mission #	AIC - AI Cargo/TRP - Troops/AIC-TRP-000 - Troops/AIC-TRP-000 - Troops.lua #	AIC - AI Cargo/TRP - Troops/AIC-TRP-000 - Troops/_unpacked/l10n/DEFAULT/AIC-TRP-000 - Troops.lua

---
+++ AIC - AI Cargo/APC - Armoured Personnel Carrier/AIC-APC-001 - Troops Relocate APC/AIC-APC-001 - Troops Relocate APC.lua
@@ -0,0 +1,26 @@
+---
+-- Name: AIC-APC-001 - Troops Relocate APC
+-- Author: FlightControl
+-- Date Created: 07 Apr 2018
+-- 
+-- Demonstration of troops relocation when carrier is destroyed...
+-- Carrier will relocate to the rescue carrier.
+
+local InfantryGroup = GROUP:FindByName( "Infantry" )
+
+local InfantryCargo = CARGO_GROUP:New( InfantryGroup, "Engineers", "Infantry Engineers", 2000 )
+
+local CargoCarrier = UNIT:FindByName( "Carrier" )
+
+CargoTroops = AI_CARGO_APC:New( CargoCarrier, InfantryCargo, 500 )
+
+
+function CargoTroops:OnAfterDestroyed( CargoCarrier )
+  CargoTroops:F( { Destroyed = CargoCarrier } )
+  -- The coordinate is passed where the carrier is destroyed.
+  local NewCarrier = self:FindCarrier( CargoCarrier:GetCoordinate(), 1000 ) -- which returns one Carrier GROUP object or nil.
+  if NewCarrier then
+    self:SetCarrier( NewCarrier )
+  end
+end
+
