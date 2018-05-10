0bd3b883af2f0338be91ce979beb7fb67119a7dc
FlightControl_Master <sven.van.de.velde@telenet.be>
2018-05-10 23:17:49 +0200

    New mission testing AI_CARGO_DISPATCHER_HELICOPTER

---
+++ AIC - AI Cargo/HEL - Helicopter/AIC-HEL-100 - AI Dispatcher Helicopter - Pickup and Deploy/_unpacked/l10n/DEFAULT/AIC-HEL-100 - AI Dispatcher Helicopter - Pickup and Deploy.lua
@@ -0,0 +1,11 @@
+---
+-- Name: AIC-HEL-100 - AI Dispatcher Helicopter - Pickup and Deploy
+-- Author: FlightControl
+-- Date Created: 10 May 2018
+--
+
+local SetCargoInfantry = SET_CARGO:New():FilterTypes( "Infantry" ):FilterStart()
+local SetHelicopter = SET_GROUP:New():FilterPrefixes( "Helicopter" ):FilterStart()
+local SetDeployZones = SET_ZONE:New():FilterPrefixes( "Deploy" ):FilterStart()
+
+AICargoDispatcherHelicopter = AI_CARGO_DISPATCHER_HELICOPTER:New( SetHelicopter, SetCargoInfantry, SetDeployZones ) 
