0394d0369bed1bb0c70413df4ce2b68d7e729771
FlightControl_Master <sven.van.de.velde@telenet.be>
2018-05-13 15:58:24 +0200

    Speed and radius settings

---
+++ AIC - AI Cargo/HEL - Helicopter/AIC-HEL-120 - AI Dispatcher Helicopter - Randomize Coordinates/AIC-HEL-120 - AI Dispatcher Helicopter - Randomize Coordinates.lua
@@ -0,0 +1,16 @@
+---
+-- Name: AIC-HEL-120 - AI Dispatcher Helicopter - Randomize Coordinates
+-- Author: FlightControl
+-- Date Created: 10 May 2018
+--
+-- Demonstrates the way how the pickup and deploy locations can be randomized.
+
+local SetCargoInfantry = SET_CARGO:New():FilterTypes( "Infantry" ):FilterStart()
+local SetHelicopter = SET_GROUP:New():FilterPrefixes( "Helicopter" ):FilterStart()
+local SetDeployZones = SET_ZONE:New():FilterPrefixes( "Deploy" ):FilterStart()
+
+AICargoDispatcherHelicopter = AI_CARGO_DISPATCHER_HELICOPTER:New( SetHelicopter, SetCargoInfantry, SetDeployZones ) 
+--AICargoDispatcherHelicopter:SetHomeZone( ZONE:FindByName( "Home" ) )
+
+AICargoDispatcherHelicopter:SetPickupRadius( 100, 50 )
+AICargoDispatcherHelicopter:SetDeployRadius( 100, 50 )
