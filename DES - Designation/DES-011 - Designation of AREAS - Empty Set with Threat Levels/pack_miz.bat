691fced739cfcc137ee66d13dcdd8b534f4d138a
FlightControl <sven.van.de.velde@telenet.be>
2018-09-22 14:02:35 +0200

    Missions

--- DES - Designation/DES-011 - Designation of AREAS - Empty Set with Threat Levels/pack_miz.bat
+++
@@ -1,2 +0,0 @@
-cd "_unpacked"
-7z a -r -y -tzip "..\DES-011 - Designation of AREAS - Empty Set with Threat Levels.miz" *
--- DES - Designation/DES-020 - Designation of AREAS - Status Menu Flash Activate/pack_miz.bat
+++
@@ -1,2 +0,0 @@
-cd "_unpacked"
-7z a -r -y -tzip "..\DES-020 - Designation of AREAS - Status Menu Flash Activate.miz" *
