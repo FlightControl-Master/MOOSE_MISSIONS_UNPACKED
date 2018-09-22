e29dfda5cf5a1927cf3d6892bbfaa88d186fe91a
FlightControl <sven.van.de.velde@telenet.be>
2018-09-22 07:28:25 +0200

    Removal of waste files corrupting missions.

--- DES - Designation/DES-100 - UNITS - Night Test/_unpacked/SADL/SETTINGS.lua
+++
@@ -1,6 +0,0 @@
-settings=
-{
-	["own_id"]=1,
-	["grp_id"]=2,
-	["callsign"]="",
-}
--- TSK - Task Modelling/TSK-210 - Helicopter - Transport Cargo Group/_unpacked/VHF_ARC_134/SETTINGS.lua
+++
@@ -1,9 +0,0 @@
-settings=
-{
-	["dials"]=
-	{
-		["mode_dial"]=1,
-		["volume"]=0.98461538553238,
-		["manual_frequency"]=116000000,
-	},
-}
