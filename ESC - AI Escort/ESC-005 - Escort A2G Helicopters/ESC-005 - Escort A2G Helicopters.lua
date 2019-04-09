8a6a758526ca664b2dabaf9f0421a281abecd38a
FlightControl <sven.van.de.velde@telenet.be>
2019-04-09 17:46:38 +0200

    new missions

---
+++ ESC - AI Escort/ESC-005 - Escort A2G Helicopters/ESC-005 - Escort A2G Helicopters.lua
@@ -0,0 +1,9 @@
+
+local FollowGroupSet = SET_GROUP:New():FilterCategories( { "helicopter" } ):FilterCoalitions( "blue" ):FilterPrefixes( { "Escort" } ):FilterStart()
+
+local LeaderUnit = UNIT:FindByName( "Leader" )
+local Escort = AI_ESCORT:New( LeaderUnit, FollowGroupSet, "Escort Attack", "Briefing" )
+Escort:FormationTrail( 100, 100 , 0 )
+Escort:Menus()
+Escort:__Start( 5 )
+
