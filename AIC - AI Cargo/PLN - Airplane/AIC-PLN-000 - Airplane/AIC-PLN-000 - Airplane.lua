61257dbc96c58d7c2fee8c0638b24f881fa4871f
Frank <frank@inter-zone.de>
2020-09-19 16:18:31 +0200

    Cargo Airplane

--- AIC - AI Cargo/PLN - Airplane/AIC-PLN-000 - Airplane/AIC-PLN-000 - Airplane.lua
+++ AIC - AI Cargo/PLN - Airplane/AIC-PLN-000 - Airplane/AIC-PLN-000 - Airplane.lua
@@ -10,6 +10,7 @@ VehicleCargoSet = SET_CARGO:New():FilterTypes( "Vehicles" ):FilterStart()
 for i = 1, 10 do
   local WorkerGroup = GROUP:FindByName( string.format( "Vehicle #%03d", i ) )
   local WorkersCargo = CARGO_GROUP:New( WorkerGroup, "Vehicles", string.format( "Vehicle %d", i ), 5000, 35 )
+  WorkersCargo:SetWeight(10000)
 end
 
 local Airplane = GROUP:FindByName( "Airplane" )
@@ -21,15 +22,16 @@ PickupAirbase = AIRBASE:FindByName( AIRBASE.Caucasus.Kobuleti )
 DeployAirbases = { AIRBASE:FindByName( AIRBASE.Caucasus.Batumi ), AIRBASE:FindByName( AIRBASE.Caucasus.Gudauta ) }
 
 
-CargoAirplane:Pickup( PickupAirbase )
+CargoAirplane:Pickup( PickupAirbase:GetCoordinate() )
 
-function CargoAirplane:onafterLoaded( Airplane, From, Event, To, Cargo )
-  CargoAirplane:Deploy( DeployAirbases[math.random( 1, #DeployAirbases ) ], math.random( 50, 250 ) )
+function CargoAirplane:OnAfterLoaded( Airplane, From, Event, To, Cargo )
+  CargoAirplane:__Deploy(0.2, DeployAirbases[math.random(#DeployAirbases)]:GetCoordinate(), math.random( 500, 750 ) )
 end
 
 
-function CargoAirplane:onafterUnloaded( Airplane, From, Event, To, Cargo )
-  CargoAirplane:Pickup( PickupAirbase, math.random( 50, 250 ) )
+--function CargoAirplane:OnAfterUnloaded( Airplane, From, Event, To, Cargo )
+function CargoAirplane:OnAfterDeployed(Airplane, From, Event, To, DeployZone)
+  CargoAirplane:__Pickup(0.2, PickupAirbase:GetCoordinate(), math.random( 500, 750 ) )
 end
 
 
