532834d4b61202c1b3b1c7f39df293da970b14d2
Applevangelist <72444570+Applevangelist@users.noreply.github.com>
2020-12-22 12:00:57 +0100

    Corrected error in formation call

--- FOR - AI Group Formation/FOR-104 - Bomber Center Wing Formation/FOR-104 - Bomber Center Wing Formation.lua
+++ FOR - AI Group Formation/FOR-104 - Bomber Center Wing Formation/FOR-104 - Bomber Center Wing Formation.lua
@@ -3,6 +3,6 @@ local FollowGroupSet = SET_GROUP:New():FilterCategories("plane"):FilterCoalition
 FollowGroupSet:Flush()
 local LeaderUnit = UNIT:FindByName( "Leader" )
 local LargeFormation = AI_FORMATION:New( LeaderUnit, FollowGroupSet, "Center Wing Formation", "Briefing" )
-LargeFormation:FormationCenterWing( 500, 50, 0, 250, 250 )
+LargeFormation:FormationCenterWing( 500, 50, 0, 0, 250, 250 )
 LargeFormation:__Start( 1 )
 
