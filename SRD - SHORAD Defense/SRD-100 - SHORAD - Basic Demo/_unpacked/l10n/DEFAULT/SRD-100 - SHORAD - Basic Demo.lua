ea244836cd15b4b73922c80a3cc3bb4c80978594
Applevangelist <post@tniedermeier.com>
2021-05-04 10:17:11 +0200

    Update 2.7

---
+++ SRD - SHORAD Defense/SRD-100 - SHORAD - Basic Demo/_unpacked/l10n/DEFAULT/SRD-100 - SHORAD - Basic Demo.lua
@@ -0,0 +1,25 @@
+-------------------------------------------------------------------------
+-- SRD-100 - SHORAD - Basic Demo
+-------------------------------------------------------------------------
+-- Documentation
+-- 
+-- SHORAD: https://flightcontrol-master.github.io/MOOSE_DOCS/Documentation/tbd
+-- 
+-- Note: As of Feb/21, SHORAD is WIP. Needs a recent build of Moose.lua => 2.5.3
+-- 
+-------------------------------------------------------------------------
+-- Observe a set of Red SAM sites being attacked by Blue SEAD and Helicopters.
+-- Red SHORAD systems will be switched on for 10 minutes to defend against
+-- HARMSs and AGMs.
+-- Red SAMs will try to evade HARMs fired at them.
+-------------------------------------------------------------------------
+-- Date: 16 Feb 2021
+-------------------------------------------------------------------------
+
+local SamSet = SET_GROUP:New():FilterPrefixes("Blue SAM"):FilterCoalitions("blue"):FilterStart()
+-- usage: SHORAD:New(name, prefix, samset, radius, time, coalition)
+myshorad = SHORAD:New("BlueShorad", "Blue SHORAD", SamSet, 22000, 600, "blue")
+myshorad:SwitchDebug(true)
+
+-- optional - make SAMs evasive (that is, if they can move)
+mysead = SEAD:New("Blue SAM")
