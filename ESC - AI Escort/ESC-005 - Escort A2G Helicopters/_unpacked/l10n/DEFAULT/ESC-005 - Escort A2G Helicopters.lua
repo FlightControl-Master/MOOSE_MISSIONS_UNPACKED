ee05e7cdd7a624fa4f0cbf0eb1af85c7f8344241
Applevangelist <post@tniedermeier.com>
2021-05-04 09:27:26 +0200

    Updated 2.7 Moose

--- ESC - AI Escort/ESC-005 - Escort A2G Helicopters/_unpacked/l10n/DEFAULT/ESC-005 - Escort A2G Helicopters.lua
+++
@@ -1,9 +0,0 @@
-
-local FollowGroupSet = SET_GROUP:New():FilterCategories( { "helicopter" } ):FilterCoalitions( "blue" ):FilterPrefixes( { "Escort" } ):FilterStart()
-
-local LeaderUnit = UNIT:FindByName( "Leader" )
-local Escort = AI_ESCORT:New( LeaderUnit, FollowGroupSet, "Escort Attack", "Briefing" )
-Escort:FormationTrail( 100, 100 , 0 )
-Escort:MenusHelicopters()
-Escort:__Start( 5 )
-
