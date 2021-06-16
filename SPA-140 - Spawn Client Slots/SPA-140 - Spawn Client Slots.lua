ea6f858bf26fbc55b43e0205dc8fb78f678a82f1
Applevangelist <72444570+Applevangelist@users.noreply.github.com>
2021-06-16 09:43:21 +0200

    Add files via upload

---
+++ SPA-140 - Spawn Client Slots/SPA-140 - Spawn Client Slots.lua
@@ -0,0 +1,20 @@
+---
+-- Name: SPA-140 - Spawn Client Slots
+-- Author: FlightControl
+-- Date Created: 30 Aug 2017
+-- Checked in 15 dez 2020 by ZERO (doesn't work)
+--
+-- # Situation:
+--
+-- A plane will be spawned Uncontrolled and later one will be spawned Controlled.
+-- Only the Controlled plane will move, the other will remain idle at the parking spot.
+-- 
+-- # Test cases:
+-- 
+
+-- Create the SPAWN object looking for the group (template) "Plane".
+SpawnPlane = SPAWN:New( "Plane" )
+
+-- Spawn the UnControlled Group
+UnControlledPlane = SpawnPlane:SpawnClientAtAirbase( AIRBASE:FindByName( AIRBASE.Caucasus.Kutaisi ) )
+
