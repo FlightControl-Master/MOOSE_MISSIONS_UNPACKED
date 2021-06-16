c25ad4271a9b764cc694c934513f5e9c507bc3be
Applevangelist <72444570+Applevangelist@users.noreply.github.com>
2021-06-16 09:34:30 +0200

    Add files via upload

--- SPA - Spawning/SPA-019 - Ground Ops - Randomize Templates without Waypoints/SPA-019 - Ground Ops - Randomize Templates without Waypoints.lua
+++ SPA - Spawning/SPA-019 - Ground Ops - Randomize Templates without Waypoints/SPA-019 - Ground Ops - Randomize Templates without Waypoints.lua
@@ -1,7 +1,8 @@
 ---
--- Name: SPA-019 - Ground Ops - Randomize Templates with Waypoints
+-- Name: SPA-019 - Ground Ops - Randomize Templates without Waypoints
 -- Author: FlightControl
 -- Date Created: 24 Feb 2017
+-- Checked in 15 dez 2020 by ZERO
 --
 -- # Situation:
 --
@@ -11,6 +12,7 @@
 -- 
 -- 1. Observe that the ground vehicles are spawned with randomized templates.
 -- 2. Observe that the ground vehicles are spread around the spawning area and are not stacked upon each other.
+-- Note by ZERO (15/12/2020): the vehicles are spawned upon each other
 
 
 -- Tests Gudauta
--- SPA - Spawning/SPA-021 - Ground Ops - Scheduled Spawns Limited Keep Unit Names/_unpacked/l10n/DEFAULT/SPA-021 - Ground Ops - Scheduled Spawns Limited Keep Unit Names.lua
+++ SPA - Spawning/SPA-021 - Ground Ops - Scheduled Spawns Limited Keep Unit Names/_unpacked/l10n/DEFAULT/SPA-021 - Ground Ops - Scheduled Spawns Limited Keep Unit Names.lua
@@ -2,6 +2,7 @@
 -- Name: SPA-021 - Ground Ops - Scheduled Spawns Limited Keep Unit Names
 -- Author: FlightControl
 -- Date Created: 14 Mar 2017
+-- Checked in 15 dez 2020 by ZERO
 --
 -- # Situation:
 --
@@ -11,7 +12,7 @@
 -- 
 -- 1. Observe that the ground vehicles are spawned at the position declared within the mission editor.
 -- 2. The vehicles should spawn according the scheduler parameters.
--- 3. There should not be more than 5 groups spawned.
+-- 3. There should not be more than 5 groups spawned. (Number of units is 5 groups x 4 units in group = 20 that is a unit limits in :initLimit function in this implementation (:InitLimit( 20 , 10 )))
 -- 4. Observe the unit names, they should have the name as defined within the ME.
 
 
@@ -21,7 +22,7 @@
 Spawn_Vehicle_1 = SPAWN
   :New( "Spawn Vehicle 1" )
   :InitKeepUnitNames()
-  :InitLimit( 5, 10 )
+  :InitLimit( 20 , 10 )
   :SpawnScheduled( 5, .5 )
 
 
